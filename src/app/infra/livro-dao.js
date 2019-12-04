c9dc6620f559e4938482f8e16488607a692dbf2c
Eduardo Godoy <eduardo.godoy@agenciaeconverse.com.br>
2019-12-04 13:24:00 -0300

    adicionando gitignore

---
+++ src/app/infra/livro-dao.js
@@ -0,0 +1,106 @@
+class LivroDAO {
+    constructor(db) {
+        this._db = db;
+    }
+
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO livros (
+                    titulo, 
+                    preco,
+                    descricao
+                ) values (?,?,?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+
+                    resolve();
+                }
+            )
+        });
+    }
+
+    buscaPorId(id){
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                SELECT * FROM livros WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro){
+                        return reject('Não foi possível encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }
+            )
+        });
+    }
+
+    atualiza(livro){
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    UPDATE livros SET
+                    titulo = ?,
+                    preco = ?,
+                    descricao = ?
+                    WHERE id = ?
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao,
+                    livro.id
+                ],
+                (erro) => {
+                    if (erro){
+                        return reject('Não foi possível atualizar o livro!');
+                    }
+                    resolve ();
+                }
+            )
+        });
+    }
+
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE FROM livro WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro){
+                        return reject('Não foi possível atualizar o livro');
+                    }
+                    resolve ();
+                }
+            )
+        });
+    }
+
+    lista() {
+        return new Promise((resolve, reject) => {
+            this._db.all(
+                'SELECT * FROM livros',
+                (erro, resultados) => {
+                    if (erro) return reject('Não foi possível listar os livros!');
+
+                    return resolve(resultados);
+                }
+            )
+        });
+    }
+}
+
+module.exports = LivroDAO;
